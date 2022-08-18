8cd176afede20a8412e53c9f48c9124f5b0ebc44
Mariana Mena <maranashokry@ahoo.com>
2022-08-18 18:15:51 -0500

    New branch after module

---
+++ scripts/quotes.js
@@ -0,0 +1,28 @@
+/* --------------------------------------Export----------------------------------     */
+
+/* export  */ const dataList = [
+    {
+        quote: "code can't lie comments can.",
+    },
+    {
+        quote: "It's harder to read code than to write it.",
+    },
+    {
+        quote: "I'm not a great programmer; I'm just a good programmer with great habits.",
+    },
+    {
+        quote: "Programming is thinking not typing.",
+    },
+];
+/* 
+const getQuotes = () => {
+    const copyDataList = dataList.map((quote) => ({ ...quote }));
+    return copyDataList;
+};
+
+export default getQuotes; */
+
+export const getQuotes = () => {
+    const copyDataList = dataList.map((quote) => ({ ...quote }));
+    return copyDataList;
+};
